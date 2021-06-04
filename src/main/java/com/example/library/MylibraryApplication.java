32aa83e6a06dffec5fc5794e018bf95ca1914e11
Switernal <switernal@outlook.com>
2021-06-05 04:57:18 +0800

    更新了端口号和打包配置

--- src/main/java/com/example/library/MylibraryApplication.java
+++ src/main/java/com/example/library/MylibraryApplication.java
@@ -7,6 +7,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -15,7 +17,7 @@ import java.util.List;
 
 @MapperScan("com.example.library.mapper")
 @SpringBootApplication
-public class MylibraryApplication {
+public class MylibraryApplication extends SpringBootServletInitializer {
 
 //	@Override
 //	public void configureMessageConverters(List<HttpMessageConverter<?>> converters){
@@ -40,4 +42,8 @@ public class MylibraryApplication {
 		SpringApplication.run(MylibraryApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
+	}
 }
